3936740176f2475d5e55696bf898b08363e8f57b
Andrew Sanchez <andrewfsanchez@gmail.com>
2019-11-04 16:52:30 -0500

    init

---
+++ archive.py
@@ -0,0 +1,28 @@
+from slackeventsapi import SlackEventAdapter
+from slackclient import SlackClient
+import os
+
+# Our app's Slack Event Adapter for receiving actions via the Events API
+slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
+
+# Create a SlackClient for your bot to use for Web API requests
+slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+slack_client = SlackClient(slack_bot_token)
+
+# Example responder to greetings
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    message = event_data["event"]
+    
+    # Prints lates message to console
+    print(message.get('text'))
+
+# Error events
+@slack_events_adapter.on("error")
+def error_handler(err):
+    print("ERROR: " + str(err))
+
+# Once we have our event listeners configured, we can start the
+# Flask server with the default `/events` endpoint on port 3000
+slack_events_adapter.start(port=3000)
