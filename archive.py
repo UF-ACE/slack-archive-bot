ca4b8e0d0982ac52eab78ff5f4f18383e839e0d8
andrewfsanchez <andrewsanchez@ufl.edu>
2019-11-08 22:54:50 +0000

    changed ip

--- archive.py
+++ archive.py
@@ -25,4 +25,4 @@ def error_handler(err):
 
 # Once we have our event listeners configured, we can start the
 # Flask server with the default `/events` endpoint on port 3000
-slack_events_adapter.start(port=3000)
+slack_events_adapter.start(port=3000, host='0.0.0.0')
